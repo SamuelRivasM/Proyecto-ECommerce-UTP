15b633b357c9160e63c86ff3d7155b82bdbe94fd
SergioChiquinta <serchivera76@gmail.com>
2025-12-01 02:23:43 -0500

    Error: Nodemailer en Render Configurado

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -157,7 +157,9 @@ exports.forgotPassword = async (req, res) => {
     // Enviar correo o SMS
     if (method === "email") {
       const transporter = nodemailer.createTransport({
-        service: "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // IMPORTANTE
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
